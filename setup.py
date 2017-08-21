adee1724f499f0c031977304c4b2a90565bd57a5
M. Eric Irrgang <ericirrgang@gmail.com>
2017-08-21 15:02:07 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gmx',
+    version='0.0.1',
+    packages=['gmx'],
+    url='',
+    license='',
+    author='M. Eric Irrgang',
+    author_email='ericirrgang@gmail.com',
+    description='Python module for GROMACS API'
+)
