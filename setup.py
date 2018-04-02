d16a7e17afeee35ce7e11756632f3e266a10fc23
M. Eric Irrgang <mei2n@virginia.edu>
2018-04-02 16:53:14 -0500

    Update setup.py
    fetch an up-to-date copy of the gromacs-gmxapi master branch

--- setup.py
+++ setup.py
@@ -208,7 +208,7 @@ class CMakeGromacsBuild(build_ext):
         # Linking is a pain because the package is relocated to the site-packages directory. We should really do this
         # in two stages.
         if build_gromacs:
-            gromacs_url = "https://github.com/kassonlab/gromacs-gmxapi/archive/v0.0.4.zip"
+            gromacs_url = "https://github.com/kassonlab/gromacs-gmxapi/archive/master.zip"
             gmxapi_DIR = os.path.join(extdir, 'data/gromacs')
             if build_for_readthedocs:
                 extra_cmake_args = ['-DCMAKE_INSTALL_PREFIX=' + gmxapi_DIR,
