9ae9b2856724b3c2505751edfde583b71abbc50c
M. Eric Irrgang <ericirrgang@gmail.com>
2018-07-16 21:56:47 +0300

    Incorporate miscellaneous documentation updates.

--- docs/reference.rst
+++ docs/reference.rst
@@ -2,6 +2,9 @@
 gmx Python module reference
 ===========================
 
+.. contents:: :local:
+    :depth: 2
+
 .. Concise reference documentation extracted directly from code.
 .. For new and non-backwards-compatible features, API versions must be given.
 
@@ -12,10 +15,10 @@ while mapping to lower level interfaces that may take a while to sort out. The
 separation also serves as a reminder that different execution contexts may be
 implemented quite diffently, though Python scripts using only the high-level
 interface should execute on all. Bindings to the ``libgromacs`` C++ API are
-provided in the submodule :py:mod:`gmx.core`.
+provided in the submodule :mod:`gmx.core`.
 
 The following documentation is extracted from the ``gmx`` Python module and is also available
-directly, using either ``pydoc`` from the command line or ``help()`` from within Python, such
+directly, using either ``pydoc`` from the command line or :py:func:`help` from within Python, such
 as during an interactive session.
 
 Refer to the Python source code itself for additional clarification.
@@ -26,10 +29,16 @@ Refer to the Python source code itself for additional clarification.
     import gmx
     from gmx.data import tpr_filename
 
-.. automodule:: gmx
+.. _python-procedural:
 
 Procedural interface
 ====================
+
+- :func:`gmx.run`
+- :func:`gmx.get_context`
+- :func:`gmx.workflow.from_tpr`
+- :func:`gmx.workflow.get_source_elements`
+- :func:`gmx.version.api_is_at_least`
 
 .. autofunction:: gmx.run
 
@@ -39,8 +48,32 @@ Procedural interface
 
 .. autofunction:: gmx.workflow.get_source_elements
 
+.. autofunction:: gmx.version.api_is_at_least
+
 Python API
 ==========
+
+.. contents:: :local:
+
+Python Classes
+--------------
+
+- :class:`gmx.workflow.WorkSpec`
+- :class:`gmx.workflow.WorkElement`
+- :class:`gmx.context.Context`
+- :class:`gmx.context.ParallelArrayContext`
+- :class:`gmx.status.Status`
+
+Python context managers
+-----------------------
+
+Objects implementing the Context interfaces defined in :mod:`gmx.context` implement
+the Python context manager protocol. When used in a ``with`` block, a Context
+produces a :class:`Session` object. See examples below.
+
+.. note::
+
+    :class:`Session` is not well specified in gmxapi 0.0.6.
 
 gmx.system module
 -----------------
@@ -67,6 +100,11 @@ gmx.status
 gmx.exceptions
 --------------
 .. automodule:: gmx.exceptions
+    :members:
+
+gmx.version
+-----------
+.. automodule:: gmx.version
     :members:
 
 Core API
--- src/gmx/__init__.py
+++ src/gmx/__init__.py
@@ -4,8 +4,8 @@ Providing Python access to Gromacs
 ==================================
 
 The gmx Python module provides an interface suitable for scripting GROMACS
-workflows, interactive use, or connectivity to external Python-based APIs.
-The project is hosted at https://github.com/kassonlab/gmxapi
+workflows, interactive use, or connectivity to external Python-based APIs. The
+project is hosted at https://github.com/kassonlab/gmxapi
 
 The API allows interaction with GROMACS that is decoupled from command-line
 interfaces, terminal I/O, and filesystem access. Computation and data management
@@ -13,50 +13,44 @@ are managed by the API until/unless the user explicitly requests specific data,
 such as for writing to a local file or manipulating with a tool that does not
 implement the Gromacs API.
 
-When data must be retrieved from GROMACS, efforts are made to do so as efficiently
-as possible, so the user should consult documentation for the specific GROMACS
-objects they are interested in regarding efficient access, if performance is
-critical. For instance, exporting GROMACS data directly to a numpy array can be
-much faster and require less memory than exporting to a Python list or tuple,
-and using available iterators directly can save a lot of memory versus creating
-an array and then iterating over it in two separate steps.
+When data must be retrieved from GROMACS, efforts are made to do so as
+efficiently as possible, so the user should consult documentation for the
+specific GROMACS objects they are interested in regarding efficient access, if
+performance is critical. For instance, exporting GROMACS data directly to a
+numpy array can be much faster and require less memory than exporting to a
+Python list or tuple, and using available iterators directly can save a lot of
+memory versus creating an array and then iterating over it in two separate
+steps.
 
-For more efficient iterative access to GROMACS data, such as analyzing a simulation
-in progress, or applying Python analysis scripts in a trajectory analysis workflow,
-consider using an appropriate call-back or, better yet, creating a C++ plugin
-that can be inserted directly in the tool chain.
+For more efficient iterative access to GROMACS data, such as analyzing a
+simulation in progress, or applying Python analysis scripts in a trajectory
+analysis workflow, consider using an appropriate call-back or, better yet,
+creating a C++ plugin that can be inserted directly in the tool chain.
 
 For more advanced use, the module provides means to access or manipulate GROMACS
-more granularly than the command-line tool. This allows rapid prototyping of
-new methods, debugging of unexpected simulation behavior, and adaptive workflows.
+more granularly than the command-line tool. This allows rapid prototyping of new
+methods, debugging of unexpected simulation behavior, and adaptive workflows.
 
 Installation
 ------------
 
-Download the repository from https://github.com/kassonlab/gmxapi and refer
-to `docs/install.rst <./install.html>`_ for details on installing this Python module.
+The gmxapi library for GROMACS must be installed to build and install the gmx
+Python module. Retrieve the GROMACS fork from
+https://github.com/kassonlab/gromacs-gmxapi and do a normal CMake build and
+install.
 
-The gmxapi library must be installed to build and install the gmx module.
-Retrieve the GROMACS fork from https://github.com/kassonlab/gromacs-gmxapi and
-do a normal CMake build and install.
+Then, download the repository from https://github.com/kassonlab/gmxapi and refer
+to `docs/install.rst <./install.html>`_ for details on installing this Python
+module.
 
 Packaging
 ---------
 
-There are a lot of open questions about packaging gmxpy. Many users will find it handy or essential to be able to
-install with distutils (setup.py), pip, or conda, in order to use arbitrary third-party Python tools in their workflow,
-so we need to be able to build the C++ extension against an already-installed GROMACS for an arbitrary Python interpreter.
-But it seems likely that system administrators might install the package for at least one or two Python installations to
-be managed with a modules system on an HPC cluster, and we would encourage this as a good time for testing and building
-docs, but after that the full distribution and CMake process may be unnecessary.
-
-I'm leaning towards separating the C++ and pure Python aspects of gmxpy (currently gmx and gmx.core) such that the full
-distribution of gmx.core is entirely CMake-based with testing and doxygen builds. It could be installed into a GROMACS
-installation path instead of the site-packages directory for a given interpreter and installation could include sufficient
-source code from which to rebuild for a different interpreter, though I'm not sure if it should be installed into a
-site-packages directory. The pure Python gmx package can then just regard gmxCore as an external package (maybe not even
-a hard dependency) and freely installed into site-packages directories. It may require some non-standard facilities to
-inform the user of its dependency or to find and build gmxCore in non-confusing, non-error-prone way.
+This Python package is built with CMake, but attempts to flexibly handle the
+user's choice of Python installation. Python virtual environments are recommended.
+If you have trouble installing this software in a Python virtual environment and
+find the accompanying documentation inadequate, please open an issue ticket at
+https://github.com/kassonlab/gmxapi/issues
 """
 
 from __future__ import absolute_import
