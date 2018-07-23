075032757f46e6fa532bbd78a8268e347efd8e64
M. Eric Irrgang <ericirrgang@gmail.com>
2018-07-23 12:47:50 +0300

    De-obfuscate gmx.status.Status docstring.

--- src/gmx/status.py
+++ src/gmx/status.py
@@ -1,9 +1,10 @@
 class Status(object):
-    """Ultimately hold status for API operation.
+    """Status for API operation.
 
-    Either this should be inherited by gmx.core.Status or a reference to it or it gets
-    much more complicated to maintain the transition between Python and C++ implementation
-    code and a consistent interface.
+    As of gmxapi 0.0.6, there is not a clear mapping between gmx.status.Status
+    and gmx.core.Status.
+
+    Reference https://github.com/kassonlab/gmxapi/issues/121
     """
     def __init__(self, success=True):
         self.success = bool(success)
