160147a0900e0272a368a62e4df4783ee989dd05
M. Eric Irrgang <ericirrgang@gmail.com>
2018-10-31 16:09:32 +0300

    Updates to CI testing.

--- ci_scripts/install_gromacs_2019.sh
+++ ci_scripts/install_gromacs_2019.sh
@@ -5,23 +5,24 @@ export GMX_DOUBLE=OFF
 export GMX_MPI=OFF
 export GMX_THREAD_MPI=ON
 
+export CCACHE_DIR=$HOME/.ccache_gmxapi
+ccache -s
+
 pushd $HOME
- [ -d gromacs-gmxapi ] || git clone --depth=1 --no-single-branch https://github.com/eirrgang/gromacs-gmxapi.git gromacs-gmxapi
+ [ -d gromacs-gmxapi ] || git clone --depth=1 --no-single-branch https://github.com/gromacs/gromacs.git gromacs-gmxapi
  pushd gromacs-gmxapi
   git branch -a
-# TODO: change back to devel after gromacs-gmxapi fork is updated.
-  git checkout port_master
+  git checkout release-2019
   pwd
   rm -rf build
   mkdir build
   pushd build
-   cmake -DGMX_BUILD_HELP=OFF \
-         -DCMAKE_CXX_COMPILER=$CXX \
+   cmake -DCMAKE_CXX_COMPILER=$CXX \
          -DCMAKE_C_COMPILER=$CC \
          -DGMX_DOUBLE=$GMX_DOUBLE \
          -DGMX_MPI=$GMX_MPI \
          -DGMX_THREAD_MPI=$GMX_THREAD_MPI \
-         -DCMAKE_INSTALL_PREFIX=$HOME/install/gromacs_devel \
+         -DCMAKE_INSTALL_PREFIX=$HOME/install/gromacs_2019 \
          ..
    make -j2 install
   popd
