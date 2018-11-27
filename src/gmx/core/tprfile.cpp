933a1581c4bcc03b63a0832f3d64b4e750bafcdf
M. Eric Irrgang <ericirrgang@gmail.com>
2018-11-27 14:02:42 +0300

    Begin to flesh out TprFile class(es)
    * Add initial GmxMdParams stuff
    * Export basic bindings for "TprFile" -> TprFileHandle and
      "read_tprfile" -> &readTprFile

--- src/gmx/core/tprfile.cpp
+++ src/gmx/core/tprfile.cpp
@@ -7,6 +7,10 @@
 
 #include "tprfile.h"
 
+#include <map>
+#include <memory>
+#include <string>
+
 #include "gromacs/mdtypes/inputrec.h"
 #include "gromacs/topology/topology.h"
 #include "gromacs/mdtypes/state.h"
@@ -17,8 +21,324 @@
 #include "gromacs/utility/cstringutil.h"
 #include "gromacs/utility/programcontext.h"
 
+#include "exceptions.h"
+#include "mdparams.h"
 
-bool gmxpy::copy_tprfile(std::string infile, std::string outfile, double until_t) {
+namespace gmxapicompat
+{
+
+
+/*!
+ * \brief Static map of GROMACS 2019 mdp file entries to normalized "type".
+ *
+ * \return
+ */
+const std::map<std::string, GmxapiType> simulationParameterTypeMap()
+{
+    return {
+            {"integrator", GmxapiType::gmxString},
+            {"tinit",      GmxapiType::gmxFloat64},
+            {"dt",         GmxapiType::gmxFloat64},
+            {"nsteps",     GmxapiType::gmxInt64},
+            {"init-step",     GmxapiType::gmxInt64},
+            {"simulation-part",     GmxapiType::gmxInt64},
+            {"comm-mode",     GmxapiType::gmxString},
+            {"nstcomm",     GmxapiType::gmxInt64},
+            {"comm-grps",     GmxapiType::gmxMDArray}, // Note: we do not have processing for this yet.
+            {"bd-fric",     GmxapiType::gmxFloat64},
+            {"ld-seed",     GmxapiType::gmxInt64},
+            {"emtol",     GmxapiType::gmxFloat64},
+            {"emstep",     GmxapiType::gmxFloat64},
+            {"niter",     GmxapiType::gmxInt64},
+            {"fcstep",     GmxapiType::gmxFloat64},
+            {"nstcgsteep",     GmxapiType::gmxInt64},
+            {"nbfgscorr",     GmxapiType::gmxInt64},
+            {"rtpi",     GmxapiType::gmxFloat64},
+            {"nstxout",     GmxapiType::gmxInt64},
+            {"nstvout",     GmxapiType::gmxInt64},
+            {"nstfout",     GmxapiType::gmxInt64},
+            {"nstlog",     GmxapiType::gmxInt64},
+            {"nstcalcenergy",     GmxapiType::gmxInt64},
+            {"nstenergy",     GmxapiType::gmxInt64},
+            {"nstxout-compressed",     GmxapiType::gmxInt64},
+            {"compressed-x-precision",     GmxapiType::gmxFloat64},
+//            {"compressed-x-grps",     GmxapiType::gmxMDArray},
+//            {"energygrps",     GmxapiType::gmxInt64},
+            {"cutoff-scheme",     GmxapiType::gmxString},
+            {"nstlist",     GmxapiType::gmxInt64},
+            {"ns-type",     GmxapiType::gmxString},
+            {"pbc",     GmxapiType::gmxString},
+            {"periodic-molecules",     GmxapiType::gmxBool},
+//            ...
+
+    };
+};
+
+/*
+ * Visitor for predetermined known types.
+ *
+ * Development sequence:
+ * 1. map pointers
+ * 2. map setters ()
+ * 3. template the Visitor setter for compile-time extensibility of type and to prune incompatible types.
+ * 4. switch to Variant type for handling (setter templated on caller input)
+ * 5. switch to Variant type for input as well? (Variant in public API?)
+ */
+
+const std::map<std::string, bool t_inputrec::*> boolParams()
+{
+    return {
+            {"periodic-molecules", &t_inputrec::bPeriodicMols},
+//            ...
+    };
+}
+
+const std::map<std::string, int t_inputrec::*> int32Params()
+{
+    return {
+            {"simulation-part",     &t_inputrec::simulation_part},
+            {"nstcomm",     &t_inputrec::nstcomm},
+            {"niter",     &t_inputrec::niter},
+            {"nstcgsteep",     &t_inputrec::nstcgsteep},
+            {"nbfgscorr",     &t_inputrec::nbfgscorr},
+            {"nstxout",     &t_inputrec::nstxout},
+            {"nstvout",     &t_inputrec::nstvout},
+            {"nstfout",     &t_inputrec::nstfout},
+            {"nstlog",     &t_inputrec::nstlog},
+            {"nstcalcenergy",     &t_inputrec::nstcalcenergy},
+            {"nstenergy",     &t_inputrec::nstenergy},
+            {"nstxout-compressed",     &t_inputrec::nstxout_compressed},
+            {"nstlist",     &t_inputrec::nstlist},
+//            ...
+    };
+}
+
+const std::map<std::string, float t_inputrec::*> float32Params()
+{
+    return {
+            {"bd-fric",     &t_inputrec::bd_fric},
+            {"emtol",     &t_inputrec::em_tol},
+            {"emstep",     &t_inputrec::em_stepsize},
+            {"fcstep",     &t_inputrec::fc_stepsize},
+            {"rtpi",     &t_inputrec::rtpi},
+            {"compressed-x-precision",     &t_inputrec::x_compression_precision},
+//            ...
+
+    };
+}
+const std::map<std::string, double t_inputrec::*> float64Params()
+{
+    return {
+            {"dt", &t_inputrec::delta_t},
+            {"tinit", &t_inputrec::init_t},
+//            ...
+
+    };
+}
+const std::map<std::string, int64_t t_inputrec::*> int64Params()
+{
+    return {
+            {"nsteps",     &t_inputrec::nsteps},
+            {"init-step",     &t_inputrec::init_step},
+            {"ld-seed",     &t_inputrec::ld_seed},
+//            ...
+
+    };
+}
+
+/*!
+ * \brief Static mapping of parameter names to gmxapi types for GROMACS 2019.
+ *
+ * \param name MDP entry name.
+ * \return enumeration value for known parameters.
+ *
+ * \throws gmxapi_compat::ValueError for parameters with no mapping.
+ */
+GmxapiType mdParamToType(const std::string& name)
+{
+    const auto staticMap = simulationParameterTypeMap();
+    auto entry = staticMap.find(name);
+    if(entry == staticMap.end())
+    {
+        throw ValueError("Named parameter has unknown type mapping.");
+    }
+    return entry->second;
+};
+
+
+/*!
+ * \brief Handle / manager for GROMACS MM computation input parameters.
+ *
+ * Interface should be consistent with MDP file entries, but data maps to TPR
+ * file interface. For type safety and simplicity, we don't have generic operator
+ * accessors. Instead, we have templated accessors that throw exceptions when
+ * there is trouble.
+ *
+ * When MDP input is entirely stored in a key-value tree, this class can be a
+ * simple adapter or wrapper. Until then, we need a manually maintained mapping
+ * of MDP entries to TPR data.
+ *
+ * Alternatively, we could update the infrastructure used by list_tpx to provide
+ * more generic output, but our efforts may be better spent in updating the
+ * infrastructure for the key-value tree input system.
+ */
+class GmxMdParams final
+{
+public:
+    /*!
+     * \brief Create an initialized but empty parameters structure.
+     *
+     * Parameter keys are set at construction, but all values are empty. This
+     * allows the caller to check for valid parameter names or their types,
+     * while allowing the consuming code to know which parameters were explicitly
+     * set by the caller.
+     *
+     * To load values from a TPR file, see getMdParams().
+     */
+//    GmxMdParams();
+
+    /*!
+     * \brief Get the current list of keys.
+     *
+     * \return
+     */
+    std::vector<std::string> keys() const
+    {
+        std::vector<std::string> keyList;
+        for (auto&& entry : int64Params_)
+        {
+            keyList.emplace_back(entry.first);
+        }
+        for (auto&& entry : floatParams_)
+        {
+            keyList.emplace_back(entry.first);
+        }
+        return keyList;
+    };
+
+    template<typename T> T extract(const std::string& key) const
+    {
+        auto value = T();
+        // should be an APIError
+        throw TypeError("unhandled type");
+    }
+
+private:
+    // TODO: update to gmxapi named types?
+    std::map<std::string, int64_t t_inputrec::*> int64Params_;
+    std::map<std::string, int t_inputrec::*> intParams_;
+    std::map<std::string, float t_inputrec::*> floatParams_;
+    std::map<std::string, double t_inputrec::*> float64Params_;
+    // t_inputrec requires libgromacs to construct or destroy.
+    t_inputrec inputRecord_;
+};
+
+template<>
+int GmxMdParams::extract<int>(const std::string& key) const {
+    const auto& params = intParams_;
+    const auto& entry = params.find(key);
+    if (entry == params.cend())
+    {
+        throw KeyError("Parameter of the requested name and type not available.");
+    }
+    else
+    {
+        const auto& dataMemberPointer = entry->second;
+        return inputRecord_.*dataMemberPointer;
+    }
+}
+template<>
+int64_t GmxMdParams::extract<int64_t>(const std::string& key) const {
+    const auto& params = int64Params_;
+    const auto& entry = params.find(key);
+    if (entry == params.cend())
+    {
+        throw KeyError("Parameter of the requested name and type not available.");
+    }
+    else
+    {
+        const auto& dataMemberPointer = entry->second;
+        return inputRecord_.*dataMemberPointer;
+    }
+}
+template<>
+float GmxMdParams::extract<float>(const std::string& key) const {
+    const auto& params = floatParams_;
+    const auto& entry = params.find(key);
+    if (entry == params.cend())
+    {
+        throw KeyError("Parameter of the requested name and type not available.");
+    }
+    else
+    {
+        const auto& dataMemberPointer = entry->second;
+        return inputRecord_.*dataMemberPointer;
+    }
+}
+template<>
+double GmxMdParams::extract<double>(const std::string& key) const {
+    const auto& params = float64Params_;
+    const auto& entry = params.find(key);
+    if (entry == params.cend())
+    {
+        throw KeyError("Parameter of the requested name and type not available.");
+    }
+    else
+    {
+        const auto& dataMemberPointer = entry->second;
+        return inputRecord_.*dataMemberPointer;
+    }
+}
+
+class TprFile
+{
+public:
+    explicit TprFile(const std::string& infile) :
+        irInstance{std::make_unique<t_inputrec>()},
+        mtop{std::make_unique<gmx_mtop_t>()},
+        state{std::make_unique<t_state>()}
+    {
+        t_inputrec *ir = irInstance.get();
+        read_tpx_state(infile.c_str(), ir, state.get(), mtop.get());
+    }
+    ~TprFile() = default;
+    TprFile(TprFile&& source) noexcept = default;
+    TprFile& operator=(TprFile&&) noexcept = default;
+
+private:
+    // These types are not moveable in GROMACS 2019, so we use unique_ptr as a
+    // moveable wrapper to let TprFile be moveable.
+    std::unique_ptr<t_inputrec>  irInstance;
+    std::unique_ptr<gmx_mtop_t>        mtop;
+    std::unique_ptr<t_state>           state;
+
+};
+
+TprFileHandle readTprFile(const std::string& filename) {
+    auto tprfile = gmxapicompat::TprFile(filename);
+    auto handle = gmxapicompat::TprFileHandle(std::move(tprfile));
+    return handle;
+}
+
+TprFileHandle::TprFileHandle(std::shared_ptr<TprFile> tprFile) :
+    tprFile_{std::move(tprFile)}
+{
+}
+
+TprFileHandle::TprFileHandle(TprFile &&tprFile) :
+    TprFileHandle{std::make_shared<TprFile>(std::move(tprFile))}
+{
+}
+
+// defaulted here to delay definition until after member types are defined.
+TprFileHandle::~TprFileHandle() = default;
+
+} // end namespace gmxapicompat
+
+namespace gmxpy
+{
+
+bool copy_tprfile(std::string infile, std::string outfile, double until_t) {
     bool success = false;
 
     const char * top_fn = infile.c_str();
@@ -55,3 +375,5 @@ bool gmxpy::copy_tprfile(std::string infile, std::string outfile, double until_t
     success = true;
     return success;
 }
+
+} // end namespace gmxpy
--- src/gmx/core/tprfile.h
+++ src/gmx/core/tprfile.h
@@ -10,8 +10,193 @@
 
 #include <string>
 
+#include "exceptions.h"
+#include "mdparams.h"
+
+namespace gmxapicompat {
+
+/*!
+ * \brief Facade for objects that can provide atomic data for a configuration.
+ */
+class StructureSource;
+
+/*!
+ * \brief Facade for objects that can provide molecular topology information for a structure.
+ */
+class TopologySource;
+
+/*!
+ * \brief Proxy to simulation state data.
+ */
+class SimulationState;
+
+/*!
+ * \brief Manager for TPR file resources.
+ *
+ * Manager object should be shared by all users of resource associated with a
+ * particular file.
+ *
+ * Multiple read-only handles may be issued if there are no write-handles.
+ * One write handle may be issued if there are no other open handles.
+ *
+ * A const TprFile may only issue read file-handles, allowing handles to be
+ * issued more quickly by avoiding atomic resource locking.
+ *
+ * \note Shared ownership of file manager could be avoided if owned by a Context.
+ * It is appropriate for a Context to own and mediate access to the manager because
+ * it provides the filesystem abstraction and in order to more intelligently
+ * map named file paths to resources. For now, TprFileHandles share ownership
+ * of the TprFile manager object via shared_ptr.
+ */
+class TprFile;
+
+/*!
+ * \brief Handle for a TPR file resource.
+ *
+ * Can provide StructureSource, TopologySource, GmxMdParams, and SimulationState
+ */
+class TprFileHandle
+{
+public:
+    explicit TprFileHandle(std::shared_ptr<TprFile> tprFile);
+    explicit TprFileHandle(TprFile&& tprFile);
+    ~TprFileHandle();
+private:
+    std::shared_ptr<TprFile> tprFile_;
+};
+
+TprFileHandle readTprFile(const std::string& filename);
+
+/*!
+ * \brief
+ * \param filehandle
+ * \return
+ *
+ * \todo replace with a helper template on T::topologySource() member function existence.
+ */
+
+TopologySource getTopologySource(const TprFileHandle& filehandle);
+
+/*!
+ * \brief
+ * \param filehandle
+ * \return
+ *
+ * \todo template on T::simulationState() member function existence.
+ */
+SimulationState getSimulationState(const TprFileHandle& filehandle);
+
+StructureSource getStructureSource(const TprFileHandle& filehandle);
+
+/*!
+ * \brief Get an initialized parameters structure.
+ * \param fileHandle
+ * \return
+ */
+gmxapicompat::GmxMdParams getMdParams(const TprFileHandle& fileHandle);
+
+// Anonymous namespace for a template that we want to define at file scope.
+namespace {
+/*!
+ * \brief Procedural interface for setting named parameters.
+ *
+ * \throws TypeError if parameter and value are of incompatible type.
+ * \throws KeyError if parameter is unknown.
+ *
+ * \note The logic of errors is potentially confusing.
+ */
+template<typename T>
+gmxapicompat::GmxMdParams &setParam(gmxapicompat::GmxMdParams *params,
+                                    std::string keyname,
+                                    T value) {
+    const auto paramType = gmxapicompat::mdParamToType(keyname);
+    if (paramType == gmxapicompat::GmxapiType::gmxInt64) {
+
+    } else {
+        // If parameter type is registered and we don't have handler code, this is a bug.
+        assert(paramType == gmxapicompat::GmxapiType::gmxNull);
+        throw gmxapicompat::KeyError(std::string("Unknown parameter name: ") + keyname);
+    }
+    return *params;
+}
+} // end anonymous namespace
+
+///*!
+// * \brief Extract a parameter to the requested type.
+// *
+// * \tparam T
+// * \param params
+// * \param name
+// * \return
+// *
+// * \throws KeyError if parameter cannot be mapped.
+// * \throws TypeError if parameter type cannot be converted to T.
+// */
+//template<typename T>
+//T extractParam(const gmxapi_compat::GmxMdParams& params, const std::string& name)
+//{
+//    gmxapi_compat::GmxapiType paramType;
+//    try
+//    {
+//        paramType = gmxapi_compat::mdParamToType(name);
+//    }
+//    catch(const gmxapi_compat::ValueError& e)
+//    {
+//        throw gmxapi_compat::KeyError("No parameter entry for this name and type.");
+//    }
+//
+//    // If
+//    if (paramType)
+//    {}
+//    else
+//    {}
+//
+//    return {};
+//}
+
+} // end namespace gmxapicompat
+
 namespace gmxpy
 {
+/*!
+ * \brief Get a dictionary of MDP key-value pairs from a TPR file.
+ *
+ * \param filename TPR file name
+ *
+ * \return key-value pairs from filename
+ *
+ * \todo Move this to gmxapicompat to provide abstraction for different versions.
+ * * handle GROMACS 2019, GROMACS master with and without feature
+ * * handle TPR versions from GROMACS 2018 and later
+ */
+//pybind11::dict read_mdparams(const std::string &filename) {
+//    using namespace pybind11::literals; // to bring in the `_a` literal
+//
+//    const char *topology_filename = filename.c_str();
+//    t_inputrec irInstance;
+//    t_inputrec *ir = &irInstance;
+//    gmx_mtop_t mtop;
+//    t_state state;
+//    read_tpx_state(topology_filename, ir, &state, &mtop);
+//
+//    return pybind11::dict("nsteps"_a = ir->nsteps);
+//}
+
+/*!
+ * \brief Write a TPR file using the provided input.
+ *
+ * \param filename output file name.
+ * \param parameters GROMACS input parameters
+ * \param structure atomic configuration
+ * \param topology molecular topology information
+ */
+//void write_tpr(const std::string &filename,
+//               pybind11::dict parameters,
+//               const StructureSource &structureSource,
+//               const TopologySource &topology) {
+//
+//    write_tpx_state(filename.c_str(), ir, &state, &mtop);
+//}
 
 /*!
  * \brief Copy and possibly update TPR file by name.
