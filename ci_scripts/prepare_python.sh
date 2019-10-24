465b0e86b4f25594f8b2ac24ec175b2b17ead162
M. Eric Irrgang <ericirrgang@gmail.com>
2019-10-24 16:08:53 +0300

    Update Travis-CI config.
    * Switch to newer Ubuntu image.
    * Stop building our own CMake.
    * Update Python versions and start testing Python 3.8
    * Update mpi4py install. 3.0.2 is not Python 3.8 compatible.
      Use maint branch.

--- ci_scripts/prepare_python.sh
+++ ci_scripts/prepare_python.sh
@@ -1,26 +1,22 @@
-#!/usr/bin/env bash
-#
+#!/bin/bash
+
 # Prepare the environment for the current Python interpreter. Assumes the
 # current shell environment variable PYTHON contains the path to the intended
 # Python interpreter. Assumes the environment variable MPICC is set to an
 # appropriate compiler wrapper (will be used to build mpi4py). Then this script
 # makes sure mpi4py is built with the intended compiler.
-#
-# To use, evaluate the results of this script in the root directory of the
-# gmxapi repository, i.e. the directory containing `requirements.txt`
-#
-# Example:
-#     $ export PYTHON=`which python`
-#     $ eval $(./ci_scripts/prepare_python.sh)
-#
+
+set -ev
+
+# TODO: Use a tagged release of mpi4py. 3.0.2 did not support Python 3.8.0.
 
 if [ -x "$PYTHON" ] ; then
     # Quiet output if QUIET is non-null
-    echo "$PYTHON -m pip install --upgrade pip setuptools ${QUIET:+'-q'};
-    $PYTHON -m pip install --no-cache-dir --upgrade --no-binary \":all:\" --force-reinstall mpi4py  ${QUIET:+'-q'};
-    $PYTHON -m pip install -r python_packaging/src/requirements.txt ${QUIET:+'-q'};
-    $PYTHON -m pip install -r python_packaging/requirements-docs.txt ${QUIET:+'-q'};
-    $PYTHON -m pip install -r python_packaging/requirements-test.txt ${QUIET:+'-q'}"
+    $PYTHON -m pip install --upgrade pip setuptools ${QUIET:+'-q'}
+    $PYTHON -m pip install --no-cache-dir --upgrade --no-binary \":all:\" --force-reinstall https://bitbucket.org/mpi4py/mpi4py/get/maint.tar.gz  ${QUIET:+'-q'}
+    $PYTHON -m pip install -r python_packaging/src/requirements.txt ${QUIET:+'-q'}
+    $PYTHON -m pip install -r python_packaging/requirements-docs.txt ${QUIET:+'-q'}
+    $PYTHON -m pip install -r python_packaging/requirements-test.txt ${QUIET:+'-q'}
 else
-    echo "echo Set PYTHON to a Python interpreter before using prepare_python.sh"
+    echo Set PYTHON to a Python interpreter before using prepare_python.sh
 fi
