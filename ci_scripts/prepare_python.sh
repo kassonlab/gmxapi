4067f88a2525b3c0063d550bc270db8fabe85e46
M. Eric Irrgang <ericirrgang@gmail.com>
2019-11-04 22:08:51 +0300

    Update prepare_python.sh
    mpi4py 3.0.3 is now up on pypi and includes the updates for Python 3.8

--- ci_scripts/prepare_python.sh
+++ ci_scripts/prepare_python.sh
@@ -8,12 +8,10 @@
 
 set -ev
 
-# TODO: Use a tagged release of mpi4py. 3.0.2 did not support Python 3.8.0.
-
 if [ -x "$PYTHON" ] ; then
     # Quiet output if QUIET is non-null
     $PYTHON -m pip install --upgrade pip setuptools ${QUIET:+'-q'}
-    $PYTHON -m pip install --no-cache-dir --upgrade --no-binary \":all:\" --force-reinstall https://bitbucket.org/mpi4py/mpi4py/get/maint.tar.gz  ${QUIET:+'-q'}
+    $PYTHON -m pip install --no-cache-dir --upgrade --no-binary \":all:\" --force-reinstall mpi4py ${QUIET:+'-q'}
     $PYTHON -m pip install -r python_packaging/src/requirements.txt ${QUIET:+'-q'}
     $PYTHON -m pip install -r python_packaging/requirements-docs.txt ${QUIET:+'-q'}
     $PYTHON -m pip install -r python_packaging/requirements-test.txt ${QUIET:+'-q'}
