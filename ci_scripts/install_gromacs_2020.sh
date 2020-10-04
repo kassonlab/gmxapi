6ddbe8944b1c5d8ed348064cd24fc822e3af8790
M. Eric Irrgang <ericirrgang@gmail.com>
2020-10-04 16:28:27 +0300

    Explicitly build against GROMACS 2020 on Travis-CI.

--- ci_scripts/install_gromacs_2020.sh
+++ ci_scripts/install_gromacs_2020.sh
@@ -14,21 +14,26 @@ else
     export ENABLE_CCACHE=OFF
 fi
 
-rm -rf build
-mkdir build
-pushd build
- cmake -DGMX_BUILD_HELP=OFF \
-       -DGMX_ENABLE_CCACHE=$ENABLE_CCACHE \
-       -DCMAKE_CXX_COMPILER=$CXX \
-       -DCMAKE_C_COMPILER=$CC \
-       -DGMX_DOUBLE=$GMX_DOUBLE \
-       -DGMX_MPI=$GMX_MPI \
-       -DGMX_THREAD_MPI=$GMX_THREAD_MPI \
-       -DGMXAPI=ON \
-       -DBUILD_TESTING=OFF \
-       -DCMAKE_INSTALL_PREFIX=$HOME/install/gromacs_2020 \
-       ..
- make -j2 install
+pushd $HOME
+  git clone --depth=1 -b release-2020 --single-branch https://github.com/gromacs/gromacs.git gromacs
+  pushd gromacs
+    rm -rf build
+    mkdir build
+    pushd build
+     cmake -DGMX_BUILD_HELP=OFF \
+           -DGMX_ENABLE_CCACHE=$ENABLE_CCACHE \
+           -DCMAKE_CXX_COMPILER=$CXX \
+           -DCMAKE_C_COMPILER=$CC \
+           -DGMX_DOUBLE=$GMX_DOUBLE \
+           -DGMX_MPI=$GMX_MPI \
+           -DGMX_THREAD_MPI=$GMX_THREAD_MPI \
+           -DGMXAPI=ON \
+           -DBUILD_TESTING=OFF \
+           -DCMAKE_INSTALL_PREFIX=$HOME/install/gromacs_2020 \
+           ..
+     make -j2 install
+    popd
+  popd
 popd
 
 [ "$ENABLE_CCACHE" ] && ccache -s
